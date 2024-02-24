32f53ce381aabb7690763cfb6dcdffc9c88e358a
MingJu Sun <20104599@mail.wit.ie>
2024-02-24 21:20:25 +0000

    Add new movie endpoint.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -53,7 +53,19 @@ export class RestAPIStack extends cdk.Stack {
             REGION: 'eu-west-1',
           },
         }
-        );
+      );
+    
+    const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/addMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
         
         new custom.AwsCustomResource(this, "moviesddbInitData", {
           onCreate: {
@@ -74,6 +86,8 @@ export class RestAPIStack extends cdk.Stack {
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn)
+        
         
         // REST API 
         const api = new apig.RestApi(this, "RestAPI", {
@@ -94,14 +108,25 @@ export class RestAPIStack extends cdk.Stack {
           "GET",
           new apig.LambdaIntegration(getAllMoviesFn, { proxy: true })
         );
+        // NEW
+  
 
         const movieEndpoint = moviesEndpoint.addResource("{movieId}");
         movieEndpoint.addMethod(
           "GET",
           new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
         );
-        
     
+     moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
+    );
+    
+        
+        
+        
+        
+          
         
       }
     }
